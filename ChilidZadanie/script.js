ff5fee18076ad3cdfec578f9050a02304fd064f2
Łukasz Górski <lukasz.gg13@gmail.com>
2017-10-19 23:12:15 +0200

    finish
    chilid

---
+++ ChilidZadanie/script.js
@@ -0,0 +1,135 @@
+'use strict';
+//LIKE & FOLLOWERSY
+$(function () {
+    var counter = 0;
+    $(".follow").click(function () {
+
+        counter++;
+        $(".followersy").text(counter);
+
+    });
+
+
+    var counterLike = 0;
+    $(".fa-heart-o").click(function () {
+
+        counterLike++;
+        $(".likes").text(counterLike);
+
+    });
+/////////popup///////////////////////////   
+   
+});
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+/////////HIDE 
+//
+//    $(".hide1").click(function () {
+//    $(".hide-elements").fadeOut("slow");
+//    $(this).addClass("show");
+//    $(this).removeClass("hide1");
+//    $(".show").text("Display  comments");
+//
+//
+//
+//     
+//});
+//        
+//         $(".show").click(function () {
+//        $(".hide-elements").fadeIn("slow");
+//        $(this).addClass("hide1");
+//        $(this).removeClass("show");
+//        $(".hide1").text("Hide comments");
+//   
+//
+//      
+//         
+//    });
+
+$("#hide").click(function () {
+   
+    $('.hide-elements').toggle(
+        function () {
+            $(".hide1").fadeOut("slow");
+            
+            
+ 
+        },
+        function () {
+            $('.show').fadeIn('slow');
+          
+   
+        });
+     
+    });
+
+
+//////End Hide
+
+////DISPLAY ADD COMMENT??/////////////////
+
+$("#add").click(function () {
+    console.log($("input#com").val());
+
+    //text
+    var comm = $('<div />', {
+        "class": 'post-description text-space',
+        "text": $("input#com").val(),
+    })
+    //img
+    var imageediv = $('<div />', {
+        "class": 'pull-left image',
+    })
+
+
+    var imagee = $('<img />', {
+        "class": 'img-circle avatar newawatar',
+        "src": 'spencer2.jpg',
+    })
+    // name and Time
+
+    var name = $('<span />', {
+        "class": 'youproftex',
+        "text": 'Mario Bross ',
+    })
+    var date = $('<p />', {
+        "class": 'date',
+        "text": month + "." + day + "." + year,
+    })
+
+    var space = $('<div />', {
+        "class": 'space',
+
+    })
+
+    $("#your").append(imagee);
+    $("#your").append(name);
+    $("#your").append(date);
+    $("#your").append(imageediv);
+    $("#your").append(comm);
+    $("#your").append(space);
+
+});
+
+///END oF ADD  DISPLAY COMMENT/////////////////
+
+///////////////////DATE
+var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+////////////////////////////////////////
+
+/////JSON FUNCTION
+
+function load() {
+    var mydata = JSON.parse(example);
+    $(".post-description").text(mydata[0].lorem);
+
+}
+load();
+
